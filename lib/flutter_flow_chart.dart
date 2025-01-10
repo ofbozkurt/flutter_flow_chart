b1503dc86d28d5bed85779189deeb01b887f953c
ofbozkurt <50094929+ofbozkurt@users.noreply.github.com>
2025-01-10 12:42:55 +0300

    Update flutter_flow_chart.dart

--- lib/flutter_flow_chart.dart
+++ lib/flutter_flow_chart.dart
@@ -1,9 +1,8 @@
-/// Flow Chart package library.
 library flutter_flow_chart;
 
 export 'src/dashboard.dart';
-export 'src/elements/connection_params.dart';
-export 'src/elements/flow_element.dart';
 export 'src/flow_chart.dart';
-export 'src/ui/draw_arrow.dart' show ArrowParams, ArrowStyle;
+export 'src/elements/flow_element.dart';
 export 'src/ui/grid_background.dart' show GridBackgroundParams;
+export 'src/ui/draw_arrow.dart' show ArrowParams;
+export 'src/elements/connection_params.dart';
